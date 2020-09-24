808024d9d5bd22a6493964a5e06b6cf5d369f1f6
IronZoomX <45451908+SpiderZoomX@users.noreply.github.com>
2020-09-24 21:39:23 +0800

    Update RC-car.ino

--- RC-car.ino
+++ RC-car.ino
@@ -1,17 +1,177 @@
+#include <WebServer.h>
 #include "esp_camera.h"
 #include <WiFi.h>
 #define CAMERA_MODEL_AI_THINKER
 #include "camera_pins.h"
-
+//const char* ssid = "Cloud-TPE-50";
+//const char* password = "s1645s1645";
+boolean takeNewPhoto = false;
+//const char* ssid = "Genius Monkey";
+//const char* password = "enhp4308";
 const char* ssid = "CCRoom3";
 const char* password = "cgshcgsh";
-WiFiServer server(23);
+//(電腦IP)
+WebServer server(80);
 void startCameraServer();
 
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>Robot control Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+  html {
+    font-family: Trebuchet MS;
+    display: inline-block;
+    margin: 0px auto;
+    text-align: center;
+  }
+  h1 {
+    color: #0F3376;
+    padding: 2vh;
+  }
+  p {
+    font-size: 1.5rem;
+  }
+  .button {
+    display: inline-block;
+    background-color: #008CBA;
+    border: none;
+    border-radius: 4px;
+    color: white;
+    padding: 16px 40px;
+    text-decoration: none;
+    font-size: 30px;
+    margin: 2px;
+    cursor: pointer;
+  }
+  .button3 {
+    background-color: #f44336;
+    padding: 16px 40px;
+  }
+  .button10
+  {background-color: #1ae47b;
+  padding: 16px 40px;}
+  .buttonGP
+  {background-color: #1ae47b;
+  padding: 16px 40px;
+  color: #0f2bf8;}
+  .buttonP
+  {background-color: #6a1ae4;
+  padding: 16px 40px;}
+  .buttonB
+  {background-color: #2557cc;
+  padding: 16px 40px;}
+  .buttonY
+  {background-color: #f8e00f;
+  padding: 16px 40px;
+  color: #f70ff8;}
+  
+
+   </style>
+</head>
+<body>
+  <h1>Wi-Fi Super RC Tank</h1>
+  <p>  <a href="/shoot"><button class="button buttonGP">SHOOT</button></a>
+       <a href="/kill"><button class="button button3">INSTANT KILL</button></a>
+  </p>
+  
+  <p>  <a href="/stop"><button class="button button10">STOP</button></a>
+  </p>
+  <p>
+    <a href="/forward"><button class="button">FORWARD</button></a></P>
+ <p>   <a href="/backward"><button class="button button2">BACKWARD</button></a></p>
+  <p>  <a href="/right"><button class="button buttonP">RIGHT</button></a>
+      <a href="/left"><button class="button buttonB">LEFT</button></a></P>
+      <P>
+  <form action="/get">
+    angle:<input type="text" name="Angle">
+  </form>
+  </p>
+</body>
+</html>)rawliteral";
+void Foward()
+{
+  digitalWrite(13,HIGH);
+  digitalWrite(12,LOW);
+  digitalWrite(15,HIGH);
+  digitalWrite(14,LOW);
+  //server.send(200, "text/html", ); 
+}
+void Backward()
+{
+  digitalWrite(13,LOW);
+  digitalWrite(12,HIGH);
+  digitalWrite(15,LOW);
+  digitalWrite(14,HIGH);
+  //server.send(200, "text/html", ); 
+}
+void Stop()
+{
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(15,LOW);
+  digitalWrite(14,LOW);
+  //server.send(200, "text/html", ); 
+}
+void Left()
+{
+  digitalWrite(13,HIGH);
+  digitalWrite(12,LOW);
+  digitalWrite(15,LOW);
+  digitalWrite(14,HIGH);
+  //server.send(200, "text/html", ); 
+}
+void Right()
+{
+  digitalWrite(13,LOW);
+  digitalWrite(12,HIGH);
+  digitalWrite(15,HIGH);
+  digitalWrite(14,LOW);
+  //server.send(200, "text/html", ); 
+}
+void Shoot()
+{
+  /*digitalWrite(13,);
+  digitalWrite(12,);
+  digitalWrite(15,);
+  digitalWrite(14,);
+  server.send(200, "text/html", Shoot()); */
+}
+void Kill()
+{
+  /*
+  digitalWrite(13,);
+  digitalWrite(12,);
+  digitalWrite(15,);
+  digitalWrite(14,);
+  server.send(200, "text/html", kill()); */
+}
+void Get()
+{
+  /*String inputMessage;
+  String inputParam;
+    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+    if (request->hasParam(A)) {
+      inputMessage = request->getParam(A)->value();
+      inputParam = PARAM_INPUT_1;
+    }
+    Serial.println(inputMessage);
+    //request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
+                                     + inputParam + ") with value: " + inputMessage +
+                                     "<br><a href=\"/\">Return to Home Page</a>");*/
+}
+void openn()
+{
+  server.send(200, "text/html", index_html);
+}
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
 void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
-  Serial.println();
+  Serial.println(); 
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -44,6 +204,11 @@ void setup() {
     config.fb_count = 1;
   }
 
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
   // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
@@ -61,6 +226,10 @@ void setup() {
   //drop down frame size for higher initial frame rate
   s->set_framesize(s, FRAMESIZE_QVGA);
 
+#if defined(CAMERA_MODEL_M5STACK_WIDE)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -70,11 +239,27 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected");
 
-  startCameraServer();
+  //startCameraServer();
 
   Serial.print("Camera Ready! Use 'http://");
   Serial.print(WiFi.localIP());
   Serial.println("' to connect");
+  //WiFi.softAP(ssid, password);
+  //WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(100);
+  server.on("/", HTTP_GET, openn);
+  server.on("/forward", HTTP_GET, Foward);
+  server.on("/backward", HTTP_GET, Backward);
+  server.on("/stop", HTTP_GET, Stop);
+  server.on("/shoot", HTTP_GET, Shoot);
+  server.on("/right", HTTP_GET, Right);
+  server.on("/left", HTTP_GET, Left);
+  server.on("/shoot", HTTP_GET, Shoot);
+  server.on("/kill", HTTP_GET, Kill);
+  server.on("/get", HTTP_GET, Get);
+  server.onNotFound(handle_NotFound);
+  server.begin();
+  Serial.println("HTTP server started");
   //右後
   pinMode(13, OUTPUT); //13 IN1 //12 IN2
   pinMode(12, OUTPUT);
@@ -86,6 +271,15 @@ void setup() {
   digitalWrite(12,LOW);
   digitalWrite(15,LOW);
   digitalWrite(14,LOW);
-}
+}  
 
-void loop() {}
+void loop() {
+  server.handleClient();
+  delay(1);
+  int dir;
+  char LR;
+  int dis;
+  int v=65; //65cm/s
+  double t=0.328659303138; //1圈的秒數
+
+}
